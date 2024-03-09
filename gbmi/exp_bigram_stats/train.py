8824e2a0e07ce4ee938700b5a9f7cb4e9e6b16e3
Jason Gross <jasongross9@gmail.com>
2024-03-09 05:34:42 +0000

    bigram: Add training for small hidden dimension

--- gbmi/exp_bigram_stats/train.py
+++ gbmi/exp_bigram_stats/train.py
@@ -222,6 +222,31 @@ ABCAB6_BIGRAM1H = Config(
 )
 
 
+ABCAB6_SMALL_HIDDEN_BIGRAM1H = Config(
+    experiment=Bigram(
+        seq_length=6,
+        num_tokens=26,
+        n_heads=1,
+        d_model=16,
+        task="abcab",
+        bos=False,
+        only_strong_signal=True,
+        random_tokens_at_end=True,
+        n_train_samples=10240,
+        logging_options=ModelMatrixLoggingOptions.all(
+            use_subplots=True, add_mean_pos_to_tok=False
+        ),
+        optimizer_kwargs={"lr": 3e-4, "betas": (0.9, 0.999), "weight_decay": 1.0},
+    ),
+    seed=999,
+    deterministic=False,
+    batch_size=512,
+    train_for=(5000, "epochs"),
+    log_every_n_steps=1,
+    validate_every=(10, "epochs"),
+    validation_batch_size=1,  # we want validation right now only to log the plots
+)
+
 ABCAB_BIGRAM = Config(
     experiment=Bigram(
         seq_length=4,

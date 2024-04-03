724e4713ddfd175e597d95b5fd9645b9fee11469
Jason Gross <jgross@mit.edu>
2024-04-03 20:30:52 +0100

    EQKE-err-svd in matplotlib

--- notebooks_jason/max_of_4.py
+++ notebooks_jason/max_of_4.py
@@ -1547,8 +1547,9 @@ def display_EQKE_SVD_analysis(
     results["EQKE_err_noticks"] = fig
     results["EQKE_err_svd"] = analyze_svd(
         (W_E_query_err2 @ W_Q_err @ W_K_errT @ W_E_key_err2T),
-        descr="EQKE_err",
+        descr="EQKE_err" if title_kind == "html" else r"EQKE\_err",
         colorscale=QK_SVD_colorscale,
+        plot_with=plot_with,
         renderer=renderer,
     )
     s1 = torch.linalg.matrix_norm(

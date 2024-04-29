298089b8e7485bb9cf21da165a914006bed3ead3
Jason Gross <jasongross9@gmail.com>
2024-04-29 00:46:21 +0000

    Adjust sweep for ind head

--- gbmi/exp_indhead/run_training_abcab8_1h_sweep.py
+++ gbmi/exp_indhead/run_training_abcab8_1h_sweep.py
@@ -9,24 +9,26 @@ from gbmi.utils import set_params
 torch.manual_seed(123)
 for seed in torch.randint(0, 2**32 - 1, (20,)):
     for num_tokens in range(5, 27):
-        for ngram in (2, 3):
-            for alpha_mix_uniform in np.linspace(start=0, stop=1, num=4):
-                cfg = set_params(
-                    ABCAB8_1H,
-                    {
-                        "seed": seed.item(),
-                        "train_for": (500, "epochs"),
-                        "validate_every": (10, "epochs"),
-                        ("experiment", "summary_slug_extra"): "sweep",
-                        ("experiment", "num_tokens"): num_tokens,
-                        ("experiment", "ngram"): ngram,
-                        ("experiment", "alpha_mix_uniform"): alpha_mix_uniform,
-                    },
-                    post_init=True,
-                )
+        for task in ("abcab", "exact-ngram"):
+            for ngram in (2, 3):
+                for alpha_mix_uniform in np.linspace(start=0, stop=1, num=4):
+                    cfg = set_params(
+                        ABCAB8_1H,
+                        {
+                            "seed": seed.item(),
+                            "train_for": (500, "epochs"),
+                            "validate_every": (10, "epochs"),
+                            ("experiment", "summary_slug_extra"): "sweep",
+                            ("experiment", "num_tokens"): num_tokens,
+                            ("experiment", "ngram"): ngram,
+                            ("experiment", "alpha_mix_uniform"): alpha_mix_uniform,
+                            ("experiment", "task"): task,
+                        },
+                        post_init=True,
+                    )
 
-                print(cfg)
-                print(IndHeadTrainingWrapper.build_model(cfg).cfg)
-                _, model = train_or_load_model(cfg, force="train")
+                    print(cfg)
+                    print(IndHeadTrainingWrapper.build_model(cfg).cfg)
+                    _, model = train_or_load_model(cfg, force="train")
 
 # _, model = main(default=ABCAB8_1H, default_force="train")

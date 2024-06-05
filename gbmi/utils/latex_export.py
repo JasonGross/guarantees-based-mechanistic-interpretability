1849c183937a06b39a8fb0304482e49f276714fc
Jason Gross <jasongross9@gmail.com>
2024-06-05 23:12:52 +0000

    Strip decimals off of floats

--- gbmi/utils/latex_export.py
+++ gbmi/utils/latex_export.py
@@ -40,7 +40,7 @@ def get_precision(f: float, base: int = 10) -> int:
 
 def format_float_full_precision(f: float) -> str:
     precision = get_precision(f)
-    return f"{f:.{precision}f}".rstrip("0")
+    return f"{f:.{precision}f}".rstrip("0").rstrip(".")
 
 
 # # Test function to ensure enough precision

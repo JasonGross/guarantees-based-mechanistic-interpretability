fe56d8c29a5dbd1163b8ea23cc228c847ffb3fec
Jason Gross <jgross@mit.edu>
2024-07-30 20:45:56 -0700

    Thread include_figures and show

--- gbmi/exp_max_of_n/plot.py
+++ gbmi/exp_max_of_n/plot.py
@@ -1730,6 +1730,10 @@ def display_EQKE_SVD_analysis(
                 "WEkkPerp-hist",
             ),
         ),
+        plot_with=plot_with,
+        renderer=renderer,
+        show=show,
+        include_figures=include_figures,
     )
     # W_E_query_err2, W_Q_err, W_K_errT, W_E_key_err2T)
     results |= resample_EQKE_err_results

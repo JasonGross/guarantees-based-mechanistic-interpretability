091c686cc33fc4481fd7368106e390d0beedc9f5
Jason Gross <jgross@mit.edu>
2024-08-08 10:46:09 -0700

    Remove old code

--- gbmi/exp_modular_arithmetic/run_train_pizzas.py
+++ gbmi/exp_modular_arithmetic/run_train_pizzas.py
@@ -3,9 +3,6 @@ from gbmi.exp_modular_arithmetic.train import train_or_load_model, PIZZA_CONFIG
 from gbmi.exp_modular_arithmetic import SEEDS
 from gbmi.utils import set_params
 
-runtime, model = train_or_load_model(PIZZA_CONFIG, force="train")
-
-
 with tqdm(SEEDS, desc="Seed") as pbar:
     for seed in pbar:
         pbar.set_postfix({"seed": seed})

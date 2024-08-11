7a80f4afe7c258f958b582d515b05d2e8ee9909b
Jason Gross <jgross@mit.edu>
2024-08-11 14:57:07 -0700

    Add some code to download and save the pizza models

--- gbmi/exp_modular_arithmetic/run_train_pizzas.py
+++ gbmi/exp_modular_arithmetic/run_train_pizzas.py
@@ -1,3 +1,4 @@
+# %%
 from tqdm.auto import tqdm
 from gbmi.exp_modular_arithmetic.train import train_or_load_model, PIZZA_CONFIG
 from gbmi.exp_modular_arithmetic import SEEDS
@@ -13,3 +14,31 @@ with tqdm(SEEDS, desc="Seed") as pbar:
                 post_init=True,
             )
         )  # , force="train"
+
+# # %%
+# from pathlib import Path
+# import torch
+# import shutil
+
+# base = Path(__file__).parent
+# wandbs = (base / "artifacts").glob("*/*.pth")
+# model_base = base / "models"
+# model_base.mkdir(exist_ok=True, parents=True)
+# # %%
+# with tqdm(wandbs) as pbar:
+#     for p in pbar:
+#         cache = torch.load(p, map_location="cpu")
+#         pbar.set_postfix(
+#             {
+#                 "seed": cache["run_config"]["seed"],
+#                 "orig_name": p.name,
+#                 "suffix_drop": "-".join(p.name.split("-")[-6:]),
+#             }
+#         )
+#         shutil.copy(
+#             p,
+#             model_base
+#             / f"{'-'.join(p.name.split('-')[:-6])}-{cache['run_config']['seed']}{p.suffix}",
+#         )
+#     # break
+# # %%

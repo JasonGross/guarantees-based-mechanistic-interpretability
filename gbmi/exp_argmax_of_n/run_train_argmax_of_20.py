176f0ea882c18c8698f50b36c8f9931e960b0d0a
Jason Gross <jgross@mit.edu>
2024-08-21 00:57:51 -0700

    Add --seeds to training for argmax

--- gbmi/exp_argmax_of_n/run_train_argmax_of_20.py
+++ gbmi/exp_argmax_of_n/run_train_argmax_of_20.py
@@ -1,8 +1,20 @@
+from argparse import ArgumentParser, BooleanOptionalAction
+
 from tqdm.auto import tqdm
-from gbmi.exp_argmax_of_n.train import train_or_load_model, SEEDS, ARGMAX_OF_20_CONFIG
+
+from gbmi.exp_argmax_of_n.train import ARGMAX_OF_20_CONFIG, SEEDS, train_or_load_model
+
+parser = ArgumentParser()
+parser.add_argument(
+    "--seeds",
+    type=str,
+    default=",".join(sorted(map(str, SEEDS))),
+    help="Comma-separated list of seeds to use",
+)
+args = parser.parse_args()
 
 for d_vocab in tqdm((64, 512), desc="d_vocab"):
-    with tqdm(SEEDS, desc="Seed", leave=False) as pbar:
+    with tqdm(map(int, args.seeds.split(",")), desc="Seed", leave=False) as pbar:
         for seed in pbar:
             cfg = ARGMAX_OF_20_CONFIG(seed, d_vocab=d_vocab, deterministic=False)
             pbar.set_postfix({"seed": seed, "d_vocab": d_vocab, "cfg": cfg})
--- gbmi/exp_argmax_of_n/run_train_argmax_of_4.py
+++ gbmi/exp_argmax_of_n/run_train_argmax_of_4.py
@@ -1,7 +1,19 @@
-from tqdm.auto import tqdm
-from gbmi.exp_argmax_of_n.train import train_or_load_model, SEEDS, ARGMAX_OF_4_CONFIG
+from argparse import ArgumentParser, BooleanOptionalAction
 
-with tqdm(SEEDS, desc="Seed") as pbar:
+from tqdm.auto import tqdm
+
+from gbmi.exp_argmax_of_n.train import ARGMAX_OF_4_CONFIG, SEEDS, train_or_load_model
+
+parser = ArgumentParser()
+parser.add_argument(
+    "--seeds",
+    type=str,
+    default=",".join(sorted(map(str, SEEDS))),
+    help="Comma-separated list of seeds to use",
+)
+args = parser.parse_args()
+
+with tqdm(map(int, args.seeds.split(",")), desc="Seed") as pbar:
     for seed in pbar:
         pbar.set_postfix({"seed": seed})
         runtime, model = train_or_load_model(

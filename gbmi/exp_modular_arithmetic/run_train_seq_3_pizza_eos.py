1f0227db8858e05901e5d4b96b413dbf901d306d
Jason Gross <jgross@mit.edu>
2024-08-21 17:15:34 -0700

    More neurons for seq len 3

--- gbmi/exp_modular_arithmetic/run_train_seq_3_pizza_eos.py
+++ gbmi/exp_modular_arithmetic/run_train_seq_3_pizza_eos.py
@@ -4,7 +4,11 @@ from gbmi.utils import set_params
 runtime, model = train_or_load_model(
     set_params(
         PIZZA_CONFIG,
-        {("experiment", "seq_len"): 3, ("experiment", "use_end_of_sequence"): True},
+        {
+            ("experiment", "seq_len"): 3,
+            ("experiment", "use_end_of_sequence"): True,
+            ("experiment", "d_mlp"): 1024,
+        },
         post_init=True,
     )
 )  # , force="train")

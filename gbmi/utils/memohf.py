18058ea7c60f0a2b6ceeffa3c5eb2b32af284f58
Jason Gross <jgross@mit.edu>
2024-08-25 15:50:31 -0700

    Fix for hf keys need to be str

--- gbmi/utils/memohf.py
+++ gbmi/utils/memohf.py
@@ -1,5 +1,5 @@
 from contextlib import contextmanager
-from typing import Any, Callable, Dict, cast
+from typing import Any, Callable, Dict, Optional, cast
 
 from datasets import Dataset, DatasetDict, load_dataset
 from datasets.data_files import EmptyDatasetError
@@ -15,12 +15,15 @@ def memohf(
     dataset_key: str,
     cache: Dict[str, Dict[str, Any]] = memohf_cache,
     get_hash: Callable = get_hash_ascii,
+    get_hash_mem: Optional[Callable] = None,
     print_cache_miss: bool = False,
     save: bool = True,
     **kwargs,
 ):
     """Lightweight memoziation using shelve + in-memory cache"""
     mem_db = cache.setdefault(repo_id, {}).setdefault(dataset_key, {})
+    if get_hash_mem is None:
+        get_hash_mem = get_hash
 
     @contextmanager
     def open_db():
@@ -33,14 +36,15 @@ def memohf(
 
         def delegate(*args, **kwargs):
             nonlocal data_modified  # Ensure we track modifications
-            key = get_hash((args, kwargs))
+            mkey = get_hash_mem((args, kwargs))
             try:
-                return mem_db[key]
+                return mem_db[mkey]
             except KeyError:
                 if print_cache_miss:
-                    print(f"Cache miss (mem): {key}")
+                    print(f"Cache miss (mem): {mkey}")
+                key = get_hash((args, kwargs))
                 try:
-                    mem_db[key] = db[key]
+                    mem_db[mkey] = db[key]
                 except Exception as e:
                     if isinstance(e, KeyError):
                         if print_cache_miss:
@@ -51,9 +55,9 @@ def memohf(
                         print(f"Error {e} in {dataset_key} in {repo_id} with key {key}")
                     if not isinstance(e, (KeyError, AttributeError)):
                         raise e
-                    mem_db[key] = db[key] = value(*args, **kwargs)
+                    mem_db[mkey] = db[key] = value(*args, **kwargs)
                     data_modified = True
-                return mem_db[key]
+                return mem_db[mkey]
 
         try:
             yield delegate
@@ -75,17 +79,21 @@ def uncache(
     dataset_key: str,
     cache: Dict[str, Dict[str, Any]] = memohf_cache,
     get_hash: Callable = get_hash_ascii,
+    get_hash_mem: Optional[Callable] = None,
     save: bool = True,
     load_dataset_kwargs: Dict[str, Any] = {},
     **kwargs,
 ):
     """Lightweight memoziation using shelve + in-memory cache"""
     mem_db = cache.setdefault(repo_id, {}).setdefault(dataset_key, {})
+    if get_hash_mem is None:
+        get_hash_mem = get_hash
+
+    mkey = get_hash_mem((args, kwargs))
+    if mkey in mem_db:
+        del mem_db[mkey]
 
     key = get_hash((args, kwargs))
-    if key in mem_db:
-        del mem_db[key]
-
     try:
         dataset = cast(DatasetDict, load_dataset(repo_id, **load_dataset_kwargs))
         db = cast(dict, dataset[dataset_key].to_dict())

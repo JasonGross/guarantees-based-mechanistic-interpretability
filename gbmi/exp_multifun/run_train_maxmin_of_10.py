e59bb2c2ee76899c1f248ca5a2e0b089ea5d1374
Jason Gross <jgross@mit.edu>
2024-08-29 15:07:35 -0700

    Add training runs for maxmin

---
+++ gbmi/exp_multifun/run_train_maxmin_of_10.py
@@ -0,0 +1,43 @@
+from argparse import ArgumentParser, BooleanOptionalAction
+
+from tqdm.auto import tqdm
+
+from gbmi.exp_multifun.train import MULTIFUN_OF_10_CONFIG, SEEDS, train_or_load_model
+
+parser = ArgumentParser()
+parser.add_argument(
+    "--seeds",
+    type=str,
+    default=",".join(sorted(map(str, SEEDS))),
+    help="Comma-separated list of seeds to use",
+)
+args = parser.parse_args()
+
+for d_vocab in tqdm((64, 128), desc="d_vocab"):
+    with (
+        tqdm((True, False), desc="eos", position=0) as eos_pbar,
+        tqdm(
+            map(int, args.seeds.split(",")), desc="Seed", position=1, leave=False
+        ) as pbar,
+        tqdm((2, 1), desc="n_heads", position=2, leave=False) as n_heads_pbar,
+    ):
+        for eos in eos_pbar:
+            for seed in pbar:
+                for n_heads in n_heads_pbar:
+                    cfg = MULTIFUN_OF_10_CONFIG(
+                        seed,
+                        d_vocab=d_vocab,
+                        n_heads=n_heads,
+                        use_end_of_sequence=eos,
+                        deterministic=False,
+                    )
+                    pbar.set_postfix(
+                        {
+                            "seed": seed,
+                            "d_vocab": d_vocab,
+                            "eos": eos,
+                            "n_heads": n_heads,
+                            "cfg": cfg,
+                        }
+                    )
+                    runtime, model = train_or_load_model(cfg)  # , force="train"
---
+++ gbmi/exp_multifun/run_train_maxmin_of_4.py
@@ -0,0 +1,32 @@
+from argparse import ArgumentParser, BooleanOptionalAction
+
+from tqdm.auto import tqdm
+
+from gbmi.exp_multifun.train import MULTIFUN_OF_4_CONFIG, SEEDS, train_or_load_model
+
+parser = ArgumentParser()
+parser.add_argument(
+    "--seeds",
+    type=str,
+    default=",".join(sorted(map(str, SEEDS))),
+    help="Comma-separated list of seeds to use",
+)
+args = parser.parse_args()
+
+with (
+    tqdm((True, False), desc="eos", position=0) as eos_pbar,
+    tqdm(map(int, args.seeds.split(",")), desc="Seed", position=1, leave=False) as pbar,
+    tqdm((2, 1), desc="n_heads", position=2, leave=False) as n_heads_pbar,
+):
+    for eos in eos_pbar:
+        for seed in pbar:
+            for n_heads in n_heads_pbar:
+                cfg = MULTIFUN_OF_4_CONFIG(
+                    seed,
+                    use_end_of_sequence=eos,
+                    n_heads=n_heads,
+                )
+                pbar.set_postfix(
+                    {"seed": seed, "eos": eos, "n_heads": n_heads, "cfg": cfg}
+                )
+                runtime, model = train_or_load_model(cfg)  # , force="train"

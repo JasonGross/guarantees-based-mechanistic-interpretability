0a4a3d48e868bdbb8d6eedcf9c57f6c0fd7df302
Jason Gross <jgross@mit.edu>
2024-09-25 19:55:23 -0700

    Add a file for renaming downloaded models

---
+++ notebooks_jason/wandb_models_rename.py
@@ -0,0 +1,34 @@
+# %%
+import shutil
+from pathlib import Path
+
+import torch
+from tqdm.auto import tqdm
+
+base = Path(".").resolve()
+wandbs = list((base / "artifacts").glob("*/*.pth"))
+model_base = base / "trained-models"
+model_base.mkdir(exist_ok=True, parents=True)
+
+with tqdm(wandbs) as pbar:
+    for path in pbar:
+        cache = torch.load(path, map_location="cpu")
+        # if cache["run_config"]["experiment"]["p"] not in (7, 12):
+        #     continue
+        pbar.set_postfix(
+            {
+                "seed": cache["run_config"]["seed"],
+                "orig_name": path.name,
+                "suffix_drop": "-".join(path.name.split("-")[-6:]),
+            }
+        )
+        seed = cache["run_config"]["seed"]
+        target = (
+            model_base / f"{'-'.join(path.name.split('-')[:-6])}-{seed}{path.suffix}"
+        )
+        if not target.exists():
+            shutil.copy(path, target)
+    # break
+# %%
+# gtar --transform='s|.*/||' --owner=0 --group=0 --numeric-owner -czf modular-add-7,12-pizza-no-eos-partial.tar.gz models/ModularAdd-{7,12}*attention-rate-1*no-eos*.pth
+# gtar --transform='s|.*/||' --owner=0 --group=0 --numeric-owner -czf modular-add-7,12-pizza-partial.tar.gz $(ls models/ModularAdd-{7,12}*attention-rate-1*.pth | grep -v no-eos)
